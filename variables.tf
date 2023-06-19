2e63764c6eef5362e4ef00a6460809561e43eadc
red-den <rjhernandez_1@yahoo.com>
2023-06-19 11:46:07 +0800

    New

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "instace_name" {
+  description = "Name assigned to every instance made"
+  type        = string
+  default     = "Project1_Instance"
+}
+
+#VPC
+variable "vpc_cidr" {
+  description = "VPC Cidr"
+  #type = string
+  default = "10.0.0.0/16"
+}
