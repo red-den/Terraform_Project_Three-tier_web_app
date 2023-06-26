6af618b4e83034510db1ecb21042bd18d730ccba
red-den <rjhernandez_1@yahoo.com>
2023-06-26 09:18:51 +0800

    Updated Version

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ wget https://github.com/azeezsalu/techmax/archive/refs/heads/main.zip
 unzip main.zip
 cp -r techmax-main/* /var/www/html/
 rm -rf techmax-main main.zip
-sudo aws s3 sync s3://ambulah/techmax-main /var/www/html
+#sudo aws s3 sync s3://ambulah/techmax-main /var/www/html
 systemctl enable httpd 
 systemctl start httpd
